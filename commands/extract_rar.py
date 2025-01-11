93f1250238b0d3faf7e386be90d166f8a5d93282
luisfosg <luisfosgdev@gmail.com>
2025-01-10 23:38:28 -0500

    fix: remove msg

--- commands/extract_rar.py
+++ commands/extract_rar.py
@@ -3,28 +3,23 @@ import rarfile
 
 def run():
     def descomprimir_rar():
-        # Solicita el path donde buscar los archivos .rar
         path = input("Ingresa el path donde buscar los archivos .rar: ").strip()
-        
-        # Verifica si el path ingresado es válido
+
         if not os.path.exists(path):
             print("El path ingresado no existe. Inténtalo de nuevo.")
             return
 
-        # Busca archivos .rar en el directorio
         rar_files = [f for f in os.listdir(path) if f.endswith('.rar')]
 
         if not rar_files:
             print("No se encontraron archivos .rar en el directorio proporcionado.")
             return
 
-        # Itera sobre los archivos encontrados y los descomprime
         for rar_file in rar_files:
             rar_path = os.path.join(path, rar_file)
             output_dir = os.path.join(path, rar_file.replace('.rar', ''))
             
             try:
-                # Abre el archivo .rar y extrae su contenido
                 with rarfile.RarFile(rar_path) as rf:
                     print(f"Descomprimiendo '{rar_file}' en '{output_dir}'...")
                     rf.extractall(output_dir)
